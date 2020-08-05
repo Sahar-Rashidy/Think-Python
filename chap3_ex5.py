b702b5ed20b2138c52bb03925db296d4611b426f
0-beginner <sahar.rashidy.ravary@gmail.com>
2020-08-05 12:51:46 +0430

    generalizing chap3_ex5 code

--- chap3_ex5.py
+++ chap3_ex5.py
@@ -1,25 +1,26 @@
 def test(n):
-    x=(n * 5) + 1
+    x=(n * m) + 1
     for i in range(x):
-        if i%5==0:
+        if i%m==0:
             for i in range(x):
-                if i== n*5:
+                if i== n*m:
                     print('+')
                 else:
-                    if i%5 == 0:
+                    if i%m == 0:
                         print('+',end=' ')
                     else:
                         print('-',end=' ' )
         else:
             for i in range (x):
-                if i==n*5:
+                if i==n*m:
                     print ('|')
                 else:
-                    if i%5==0:
+                    if i%m==0:
                         print('|',end=' ')
                     else:
                         print(' ',end=' ')
 
 import pyinputplus as pyip
-n= pyip.inputNum('how many rows and column?\n')
+n= pyip.inputInt('how many rows and column?\n')
+m=pyip.inputInt('how frequently di you want to see a +?\n')
 test(n)
